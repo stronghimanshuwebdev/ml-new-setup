0828adf7a4f458d05cea5310f95ac278251c701f
Himanshu kumar chaturvedi <chaturvedihimanshu120@gmail.com>
2024-01-27 23:46:40 +0530

    Updated

--- app.py
+++ app.py
@@ -11,3 +11,11 @@ if __name__ == '__main__':
 ## heroku email chaturvedihimanshu120@gmail.com
 ## heroku api key = 9db712fc-8051-4f84-8387-a4302dba93b8
 ## heroku app name = 
+    
+## command to buiild docker image
+## docker build -t<image name>:<tag name> image name must be lower case
+    ## ex = docker build -t ml project:latest .
+
+    ## to list docker image docker images
+    ## to run docker images
+    ## 
